7cefd31d736ec4f96155b17c9e84eb2f844cfac0
Augusto Saboia <134610628+Augustuuuuu@users.noreply.github.com>
2026-05-23 18:33:36 -0300

    feat(habits): optimize dashboard habit completion check and improve PHPUnit setup
    - Refactor `wasCompletedToday` logic into the `Habit` model for cleaner and reusable code.
    - Update dashboard to utilize the `wasCompletedToday` method for habit completion status.
    - Preload `habitLogs` in `HabitController` to reduce redundant queries on the dashboard.
    - Add PHPUnit configuration to project for improved test organization.
    - Include Laravel Debugbar package for enhanced debugging during development.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,12 +18,6 @@
       </h2>
       <ul class="flex flex-col gap-2">
         @forelse($habits as $item)
-          @php
-            $wasCompletedToday = $item->habitLogs
-            ->where('user_id', auth()->id())
-            ->where('completed_at', Carbon::today()->toDateString())
-            ->isNotEmpty();
-          @endphp
           <li class="habit-shadow-lg p-2 bg-[#FFDAAC]">
             <form method="POST"
                   action="{{route('habits.toggle', $item->id)}}"
@@ -33,7 +27,7 @@
               <input
                 type="checkbox"
                 class="w-5 h-5" {{$item->is_completed ? 'checked': ''}}
-                {{ $wasCompletedToday ? 'checked' : ''}}
+                {{ $item->wasCompletedToday() ? 'checked' : ''}}
                 onchange="document.getElementById('form-{{$item->id}}').submit()"
               />
               <p class="font-bold text-l">
